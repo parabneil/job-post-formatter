118b5a3e80d704239b804754b193dec624338d49
parabneil <parab.neil25@gmail.com>
2022-07-07 15:12:10 +0530

    security fixes and new design

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
       {/*<Container />*/}
       <div className="welcomePage">
         <div className="orgName1">ORNAMAX</div>
+        <div className="loader">
+          <img src="https://static.wixstatic.com/media/198a64_e7d63cee330143028e80876a61bdcf71~mv2.gif" />
+        </div>
         <div className="orgName2">WEB SOLUTIONS</div>
-      </div>
-      <div className="loader">
-        <img src="https://static.wixstatic.com/media/198a64_e7d63cee330143028e80876a61bdcf71~mv2.gif" />
       </div>
     </div>
   );
